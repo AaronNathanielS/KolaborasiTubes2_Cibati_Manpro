833d55302e6ecdfda1f2ad297151139138768ac9
AaronNathanielS <6182101020@student.unpar.ac.id>
2023-12-07 20:16:30 +0700

    Melakukan penambahan page

--- server.js
+++ server.js
@@ -5,4 +5,21 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   res.render("Home");
 });
+
+app.get("/addfile", (req, res) => {
+  res.render("AddFile");
+});
+
+app.get("/summarizedata", (req, res) => {
+  res.render("SummarizeData");
+});
+
+app.get("/grafikbar", (req, res) => {
+  res.render("GrafikBar");
+});
+
+app.get("/grafikscatter", (req, res) => {
+  res.render("GrafikScatter");
+});
+
 app.listen(3000);
