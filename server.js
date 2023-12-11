1bc3f0ba3b2d45cb09e020ace91305be204d84b3
GilbertValentinus <gilbertvalentinus@gmail.com>
2023-12-11 22:49:54 +0700

    Connect db

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require("express");
+const mysql = require("mysql");
 
+const port = 8000;
 const app = express();
 app.set("view engine", "ejs");
+
+const pool = mysql.createPool({
+  multipleStatements: true,
+  user: "root",
+  password: "",
+  database: "Manpro",
+  host: "127.0.0.1",
+  port: 3306,
+});
+
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error("Error connecting to database:", err.message);
+  } else {
+    console.log("Connected to database");
+    connection.release();
+  }
+});
+
 app.get("/", (req, res) => {
   res.render("Home");
 });
@@ -22,4 +43,8 @@ app.get("/grafikscatter", (req, res) => {
   res.render("GrafikScatter");
 });
 
-app.listen(3000);
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+
