c8968c12f39973c49197b4c8be07480046ccbcbf
GilbertValentinus <gilbertvalentinus@gmail.com>
2023-12-13 20:20:12 +0700

    perbaiki DB + fitur Add File

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.get("/grafikscatter", (req, res) => {
   res.render("GrafikScatter");
 });
 
-//file upload
+//file upload CSV/Excel
 app.post("/upload", upload.single("file"), (req, res) => {
   const fileBuffer = req.file.buffer;
   const workbook = xlsx.read(fileBuffer, { type: "buffer" });
@@ -59,57 +59,36 @@ app.post("/upload", upload.single("file"), (req, res) => {
 
   const data = xlsx.utils.sheet_to_json(worksheet);
 
-  // Insert data into the People table
-  const insertPeopleQuery = "INSERT INTO People (ID, Year_Birth, Education, Marital_Status, Income, Kidhome, Teenhome, Dt_Customer, Recency, Complain) VALUES ?";
-  const peopleValues = data.map(row => [row.ID, row.Year_Birth, row.Education, row.Marital_Status, row.Income, row.Kidhome, row.Teenhome, row.Dt_Customer, row.Recency, row.Complain]);
-  pool.query(insertPeopleQuery, [peopleValues], (err, result) => {
+  const insertDataQuery = `
+    INSERT INTO Marketing_Campaign (
+      ID, Year_Birth, Education, Marital_Status, Income, Kidhome, Teenhome,
+      Dt_Customer, Recency, MntWines, MntFruits, MntMeatProducts, MntFishProducts,
+      MntSweetProducts, MntGoldProds, NumDealsPurchases, NumWebPurchases,
+      NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth, AcceptedCmp1,
+      AcceptedCmp2, AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, Complain,
+      Z_CostContact, Z_Revenue, Response
+    ) VALUES ?`;
+
+  const values = data.map(row => [
+    row.ID, row.Year_Birth, row.Education, row.Marital_Status, row.Income,
+    row.Kidhome, row.Teenhome, row.Dt_Customer, row.Recency, row.MntWines,
+    row.MntFruits, row.MntMeatProducts, row.MntFishProducts, row.MntSweetProducts,
+    row.MntGoldProds, row.NumDealsPurchases, row.NumWebPurchases,
+    row.NumCatalogPurchases, row.NumStorePurchases, row.NumWebVisitsMonth,
+    row.AcceptedCmp1, row.AcceptedCmp2, row.AcceptedCmp3, row.AcceptedCmp4,
+    row.AcceptedCmp5, row.Complain, row.Z_CostContact, row.Z_Revenue, row.Response
+  ]);
+
+  pool.query(insertDataQuery, [values], (err, result) => {
     if (err) {
-      console.error("Error inserting data into People table:", err.message);
-      res.status(500).send("Error inserting data into the People table");
+      console.error("Error inserting data into Marketing_Campaign table:", err.message);
+      res.status(500).send("Error inserting data into the Marketing_Campaign table");
     } else {
-      console.log("Data inserted into People table successfully");
+      console.log("Data inserted into Marketing_Campaign table successfully");
+      res.redirect("/addfile?success=true"); // Redirect to the addfile page after successful upload
     }
   });
-
-  // Insert data into the Products table
-  const insertProductsQuery = "INSERT INTO Products (ID, MntWines, MntFruits, MntMeatProducts, MntFishProducts, MntSweetProducts, MntGoldProds) VALUES ?";
-  const productsValues = data.map(row => [row.ID, row.MntWines, row.MntFruits, row.MntMeatProducts, row.MntFishProducts, row.MntSweetProducts, row.MntGoldProds]);
-  pool.query(insertProductsQuery, [productsValues], (err, result) => {
-    if (err) {
-      console.error("Error inserting data into Products table:", err.message);
-      res.status(500).send("Error inserting data into the Products table");
-    } else {
-      console.log("Data inserted into Products table successfully");
-    }
-  });
-
-  // Insert data into the Promotion table
-  const insertPromotionQuery = "INSERT INTO Promotion (ID, NumDealsPurchases, AcceptedCmp1, AcceptedCmp2, AcceptedCmp3, AcceptedCmp4, AcceptedCmp5, Response) VALUES ?";
-  const promotionValues = data.map(row => [row.ID, row.NumDealsPurchases, row.AcceptedCmp1, row.AcceptedCmp2, row.AcceptedCmp3, row.AcceptedCmp4, row.AcceptedCmp5, row.Response]);
-  pool.query(insertPromotionQuery, [promotionValues], (err, result) => {
-    if (err) {
-      console.error("Error inserting data into Promotion table:", err.message);
-      res.status(500).send("Error inserting data into the Promotion table");
-    } else {
-      console.log("Data inserted into Promotion table successfully");
-    }
-  });
-
-  // Insert data into the Place table
-  const insertPlaceQuery = "INSERT INTO Place (ID, NumWebPurchases, NumCatalogPurchases, NumStorePurchases, NumWebVisitsMonth) VALUES ?";
-  const placeValues = data.map(row => [row.ID, row.NumWebPurchases, row.NumCatalogPurchases, row.NumStorePurchases, row.NumWebVisitsMonth]);
-  pool.query(insertPlaceQuery, [placeValues], (err, result) => {
-    if (err) {
-      console.error("Error inserting data into Place table:", err.message);
-      res.status(500).send("Error inserting data into the Place table");
-    } else {
-      console.log("Data inserted into Place table successfully");
-    }
-  });
-
-  res.redirect("/addfile?success=true"); // Redirect to the addfile page after successful upload
 });
-
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
